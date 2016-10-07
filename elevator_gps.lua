38ee7bae25975c784b57a3651766eb3c7cb7ecef
Stephen <srivest46@gmail.com>
2016-10-07 13:30:54 -0700

    cleand up gps code

--- elevator_gps.lua
+++ elevator_gps.lua
@@ -1,18 +1,17 @@
 rednet.open("back")
 print("Starting gps")
 while true do
+	print("Waiting for input...")
 	local ID, message, protocol = rednet.receive("elevator_gps")
+	term.clear()
+	term.setCursorPos(1,1)
 	if message == "getGPS" then
 		print("Request received!")
 		local x, y, z = gps.locate()
 		rednet.send(ID, y, "elevator_gps")
 		sleep(3)
-		term.clear()
-		term.setCursorPos(1,1)
 	else
 		print("unknown command!")
 		sleep(3)
-		term.clear()
-		term.setCursorPos(1,1)
 	end
 end
