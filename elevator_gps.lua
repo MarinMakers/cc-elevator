2fb57f646b2c61874c38f3d5f7331f51a510921b
Stephen <srivest46@gmail.com>
2016-10-07 12:33:01 -0700

    added stuff

--- elevator_gps.lua
+++ elevator_gps.lua
@@ -2,18 +2,11 @@ rednet.open("back")
 print("Starting gps")
 while true do
 	local ID, message, protocol = rednet.receive("elevator_gps")
-	if protocol == elevator then
-		if message == getGPS then
-			local x, y, z = gps.locate(5, true)
-			rednet.send(ID, y, "elevator_gps")
-		else
-			print("unknown command!")
-			sleep(2)
-			term.clear()
-			term.setCursorPos(1,1)
-		end
+	if message == getGPS then
+		local x, y, z = gps.locate(5, true)
+		rednet.send(ID, y, "elevator_gps")
 	else
-		print("unknown protocol!")
+		print("unknown command!")
 		sleep(2)
 		term.clear()
 		term.setCursorPos(1,1)
