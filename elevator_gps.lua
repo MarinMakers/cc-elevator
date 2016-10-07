622f776db1ef77aaaa1112ff91fe9304eece5ec6
Stephen <srivest46@gmail.com>
2016-10-07 12:28:11 -0700

    added stuff

--- elevator_gps.lua
+++ elevator_gps.lua
@@ -1,11 +1,11 @@
 rednet.open("back")
 print("Starting gps")
 while true do
-	local ID, message, protocol = rednet.receive()
+	local ID, message, protocol = rednet.receive("elevator_gps")
 	if protocol == elevator then
 		if message == getGPS then
 			local x, y, z = gps.locate(5, true)
-			rednet.send(ID, y, elevator)
+			rednet.send(ID, y, "elevator_gps")
 		else
 			print("unknown command!")
 			sleep(2)
--- elevator_server.lua
+++ elevator_server.lua
@@ -3,7 +3,7 @@ args[1]	= desiredLevel		--floor to sent elevator to
 print(desiredLevel)
 clientID 		= 38				--receiving computer ID
 rednet.open("back")
-rednet.send(clientID, desiredLevel, elevator)
+rednet.send(clientID, desiredLevel, "elevator")
 print(string.format("Sending elevator to %s floor.", desiredLevel))
 --sleep(3)
 term.clear()
